975de08116ae56054fc96aa0da1d061567ac707d
Luis Gil <giluis2000@gmail.com>
2024-10-16 09:16:33 -0400

    feedforward devolviend y_hat

--- feedforward.py
+++ feedforward.py
@@ -17,10 +17,22 @@ w = np.random.rand(2)
 alpha = 0.001
 
 # Generamos las series
-x, y = generate_series()
+x_serie, y_serie = generate_series(0, 0, 1000)
+x = np.column_stack((x_serie[:-1], y_serie[:-1]))
+y = x_serie[1:]
 
-def forward_prop(row):
-    y_hat = np.dot([x[row], y[row]], w) + b
+# Dividimos en conjunto de entrenamiento y prueba
+x_train, x_test = x[:800], x[800:]
+y_train, y_test = y[:800], y[800:]
+
+# Asignamos al modelo la data de entrenamiento
+x = x_train
+y = y_train
+
+
+def forward_prop(row): 
+    y_hat = np.dot(x[row], w) + b
+    return y_hat
     if y_hat > 0:
         return 1
     else:
@@ -28,45 +40,51 @@ def forward_prop(row):
 
 def backward_prop(y_hat, row):
     global w, b
-    w[0] = w[0] + alpha * (y[row] - y_hat) * x[row]
-    w[1] = w[1] + alpha * (y[row] - y_hat) * x[row]
-    b = b + alpha * (y[row] - y_hat)
+    error = y[row] - y_hat
+    w[0] = w[0] + alpha * (error) * x[row][0]
+    w[1] = w[1] + alpha * (error) * x[row][1]
+    b = b + alpha * (error)
 
-
-# Retorna la predicción de y_hat, para el conjunto de prueba.
+# Retorna la prediccion de y_hat, para el conjunto de prueba.
 def predict(x):
-    y = []
-
-    # El usuario podr´ıa ingresar varias filas.
-    # Calcule y_hat para cada una de las filas del conjunto de
-    # datos de prueba:
-
-    #Suma ponderada
-    y_pred = np.dot(row, w) + b
-
-    # Paso de la suma ponderada por la función de activación
-    if (y_pred > 0):
-        y_pred = 1
-    else:
-        y_pred = 0
+    y_pred = []
+    # y = []
     
-    y.append(y_pred)
+    # El usuario podría ingresar varias filas. Calcule y_hat para cada una de las filas
+    # del conjunto de datos de prueba.
 
-    # Devuelve la matriz de valores y_hat predichos para los datos de prueba
-    # correspondientes (x)
-    return y
+    for row in x:
+        # Suma ponderada
+        y_hat = np.dot(row, w) + b
+        y_pred.append(y_hat)
+        # Paso de la suma ponderada por la función de activación
+        # if y_pred > 0:
+        #     y.append(1)
+        # else:
+        #     y.append(0)
+    
+    # return y
+    return y_pred
 
 # * Entrenamiento
-
 # Número de épocas
 for epoch in range(1000):
 
     # Para cada fila en x (ciclo a través del conjunto de datos)
     for row in range(x.shape[0]):
 
-        # Para cada fila en x, predice y_hat
+        # Para cada fila de x, predice y_hat
         y_hat = forward_prop(row)
 
-        # Para cada fila en x, actualiza los pesos y el sesgo
+        # Para cada fila se actualiza los pesos
         backward_prop(y_hat, row)
+
 print(w, b)
+
+# * Prueba
+y_pred = predict(x_test)
+# print(y_pred)
+
+# Calculamos el error
+error = np.mean((y_test - y_pred) ** 2)
+print(error)
